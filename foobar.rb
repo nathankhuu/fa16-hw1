841abd5d16a1a76eb3503e8d2c21b2445fe1286d
Nathan Khuu <nkhuu@berkeley.edu>
2016-09-17 16:53:59 -0700

    finished

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,9 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    result = a.map {|x| (x.to_i) + 2}
+    result = result.select { |y| y % 2 == 0 and y <= 10}
+    return result.uniq.reduce(:+)
   end
 end
 
